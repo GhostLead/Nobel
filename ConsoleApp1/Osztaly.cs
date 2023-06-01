89c0e3d0b85dcb5fdbc0be98198ae04652a04c59
Mechwart András <mechwwart@gmail.com>
2023-06-01 11:06:38 +0200

    kesz

---
+++ ConsoleApp1/Osztaly.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Osztaly
+    {
+
+        int evszam;
+        string tipus;
+        string kerNev;
+        string vezNev;
+        
+
+        public Osztaly(string CSVsor)
+        {
+            string[] tomb = CSVsor.Split(';');
+
+            this.evszam = int.Parse(tomb[0]);
+            this.tipus = tomb[1];
+            this.vezNev = tomb[2];
+            this.kerNev = tomb[3];
+        }
+
+        public int Evszam { get => evszam; set => evszam = value; }
+        public string Tipus { get => tipus; set => tipus = value; }
+        public string KerNev { get => kerNev; set => kerNev = value; }
+        public string VezNev { get => vezNev; set => vezNev = value; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        static List<Osztaly> lista = new List<Osztaly>();
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("nobel.csv");
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                Osztaly o = new Osztaly(sor);
+                lista.Add(o);
+            }
+            sr.Close();
+
+            // 4. feladat
+            Console.WriteLine($"4. feladat: {lista.Find(x => x.Evszam == 2017 && x.Tipus == "irodalmi").KerNev} {lista.Find(x => x.Evszam == 2017 && x.Tipus == "irodalmi").VezNev}");
+
+            // 6. feladat
+            List<Osztaly> CurieLista = lista.Where(x => x.VezNev.Contains("Curie") || x.KerNev.Contains("Curie")).ToList();
+            Console.WriteLine("6. feladat:");
+            foreach (var item in CurieLista)
+            {
+                Console.WriteLine($"{item.Evszam}: {item.KerNev} {item.VezNev} ({item.Tipus})");
+            }
+
+            // 7. feladat
+
+            Console.WriteLine("7. feladat:");
+            Console.WriteLine($"kémiai    { lista.Count(x => x.Tipus == "kémiai")}db");
+            Console.WriteLine($"fizikai    { lista.Count(x => x.Tipus == "fizikai")}db");
+            Console.WriteLine($"orvosi    { lista.Count(x => x.Tipus == "orvosi")}db");
+            Console.WriteLine($"irodalmi    { lista.Count(x => x.Tipus == "irodalmi")}db");
+            Console.WriteLine($"béke    { lista.Count(x => x.Tipus == "béke")}db");
+            Console.WriteLine($"közgazdaságtani    { lista.Count(x => x.Tipus == "közgazdaságtani")}db");
+
+
+            // 8. feladat
+
+            StreamWriter sw = new StreamWriter("orvosi.txt");
+
+            foreach (var item in lista)
+            {
+                string sor = $"{item.Evszam}:{item.KerNev} {item.VezNev}";
+                sw.WriteLine(sor);
+            }
+            Console.WriteLine("orvosi.txt");
+            sw.Close();
+
 
 
         }
